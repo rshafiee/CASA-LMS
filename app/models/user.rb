86ac3033109dc3077f77710f8a35594af22f5efa
Rahman Shafiee <r.shafiee@gmx.de>
2021-05-25 17:56:21 +0200

    Avatar and default picture updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,29 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :registerable, :confirmable
 
   has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar
+    else
+      "/default_profile.jpg"
+    end
+  end
+
+  private
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_profile.jpg'
+            )
+          ), filename: 'default_profile.jpg',
+          content_type: 'image/jpg'
+        )
+    end
+  end
 
 # To skip required confirmation all-together
 # protected
