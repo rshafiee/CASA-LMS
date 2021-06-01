7d31107eb4f252be06726a5e2fa0e463bafc4344
Rahman Shafiee <r.shafiee@gmx.de>
2021-06-01 11:27:38 +0200

    Added columns to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar, :first_name, :last_name, :city, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :first_name, :last_name, :city, :phone])
   end
 end
