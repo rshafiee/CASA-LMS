3d886203ca59abed235d28e7462e791ea388c992
Rahman Shafiee <r.shafiee@gmx.de>
2021-06-01 11:50:11 +0200

    corrected the route in the production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://git.heroku.com/lms-casa.git" }
+  config.action_mailer.default_url_options = { host: "https://lms-casa.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
