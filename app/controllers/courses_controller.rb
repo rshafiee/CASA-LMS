3ab0a3da03c19e7503d377e9556d1d6d906a4a3d
Rahman Shafiee <r.shafiee@gmx.de>
2021-07-06 19:21:42 +0200

    Fixed the course form

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,25 +1,20 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: %i[ show edit update destroy ]
+  before_action :set_course, only: %i[show edit update destroy]
 
-  # GET /courses or /courses.json
   def index
     @courses = Course.all
   end
 
-  # GET /courses/1 or /courses/1.json
   def show
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
   end
 
-  # GET /courses/1/edit
   def edit
   end
 
-  # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
 
@@ -34,7 +29,6 @@ class CoursesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
   def update
     respond_to do |format|
       if @course.update(course_params)
@@ -47,7 +41,6 @@ class CoursesController < ApplicationController
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
   def destroy
     @course.destroy
     respond_to do |format|
@@ -57,13 +50,12 @@ class CoursesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.require(:course).permit(:title, :description, :start_date, :end_date, :start_time, :end_time, :shift, :price)
-    end
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :start_date, :end_date, :start_time, :end_time, :shift, :price)
+  end
 end
