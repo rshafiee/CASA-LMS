cbaac5ec8f9bef639ac397335debf090fd8eee89
Rahman Shafiee <r.shafiee@gmx.de>
2021-07-13 17:36:51 +0200

    Seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,14 @@
 
 # User.destroy_all
 
-# puts "Testing"
 
-# # user_one = User.create(email: "rahman@test.com", password: "123456", first_name: "Rahman", last_name: "Shafiee", city: "Berlin")
-# # user_one.save
-# puts "user_one created!"
-# course_one = Course.create(title: "A1", description: "German Course for absolute beginners", start_date: 2021-8-01, end_date: 2021-8-27, start_time: 9-00, end_time: 12-30, time_of_day: "Morning", users_id: 5)
-# course_one.save!
-# puts "course created!"
+ puts "Testing"
+
+# user_one = User.create(email: "rahman@test.com", password: "123456", first_name: "Rahman", last_name: "Shafiee", city: "Berlin")
+# user_one.save
+ puts "user_one created!"
+ # course_one = Course.create(title: "A1", description: "German Course for absolute beginners", start_date: 2021-8-01, end_date: 2021-8-27, shift: "Morning")
+ # course_one.save!
+ course_one = Course.create(title: "B2", description: "German Course for absolute beginners", start_date: DateTime.new(2021,8,01), end_date: DateTime.new(2021,8,10), start_time: Time.new.strftime("%H:%M"), end_time: Time.now.strftime("%H:%M"), shift: "Morning")
+ course_one.save!
+ puts "course created!"
