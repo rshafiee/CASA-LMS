e08ee5dea5aa7a76171f0bd07f71d68bfe5ab88b
Rahman Shafiee <r.shafiee@gmx.de>
2021-07-13 23:34:16 +0200

    Seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,50 +11,64 @@
 
  puts "Testing"
 
-# user_one = User.create(email: "rahman@rahman.com", password: "123456", first_name: "Rahman", last_name: "Shafiee", city: "Berlin")
-# user_one.save
-# user_two = User.create(email: "test@test.com", password: "123456", first_name: "Bob", last_name: "Doe", username: "Bobi", city: "Berlin")
-# user_two.save
-# user_three = User.create(email: "casa@casa.com", password: "123456", first_name: "Hank", last_name: "Mudy", username: "Hanko", city: "Berlin")
-# user_three.save
-# user_four = User.create(email: "schule@schule.com", password: "123456", first_name: "Werner", last_name: "Riebe", username: "Werner", city: "Berlin")
-# user_four.save
-# user_five = User.create(email: "school@school.com", password: "123456", first_name: "JZ", last_name: "Ayaz", username: "JZ", city: "Berlin")
-# user_five.save
+user_one = User.create(email: "rahman@rahman.com", password: "123456", first_name: "Rahman", last_name: "Shafiee", city: "Berlin")
+user_one.save
+user_two = User.create(email: "test@test.com", password: "123456", first_name: "Bob", last_name: "Doe", username: "Bobi", city: "Berlin")
+user_two.save
+user_three = User.create(email: "casa@casa.com", password: "123456", first_name: "Hank", last_name: "Mudy", username: "Hanko", city: "Berlin")
+user_three.save
+user_four = User.create(email: "schule@schule.com", password: "123456", first_name: "Werner", last_name: "Riebe", username: "Werner", city: "Berlin")
+user_four.save
+user_five = User.create(email: "school@school.com", password: "123456", first_name: "JZ", last_name: "Ayaz", username: "JZ", city: "Berlin")
+user_five.save
 puts "user_one created!"
 
-# teacher_one = Teacher.create(first_name: "Jane", last_name: "Doe")
-# teacher_one.save
-# teacher_two = Teacher.create(first_name: "John", last_name: "Doe")
-# teacher_two.save
-# teacher_three = Teacher.create(first_name: "Bob", last_name: "Doe")
-# teacher_three.save
-# teacher_four = Teacher.create(first_name: "Liz", last_name: "Doe")
-# teacher_four.save
+teacher_one = Teacher.create(first_name: "Jane", last_name: "Doe")
+teacher_one.save
+teacher_two = Teacher.create(first_name: "John", last_name: "Doe")
+teacher_two.save
+teacher_three = Teacher.create(first_name: "Bob", last_name: "Doe")
+teacher_three.save
+teacher_four = Teacher.create(first_name: "Liz", last_name: "Doe")
+teacher_four.save
 puts "teachers created"
 
-# student_one = Student.create(first_name: "Jane", last_name: "Doe")
-# student_one.save
-# student_two = Student.create(first_name: "John", last_name: "Doe")
-# student_two.save
-# student_three = Student.create(first_name: "Bob", last_name: "Doe")
-# student_three.save
-# student_four = Student.create(first_name: "Liz", last_name: "Doe")
-# student_four.save
+student_one = Student.create(first_name: "Jane", last_name: "Doe")
+student_one.save
+student_two = Student.create(first_name: "John", last_name: "Doe")
+student_two.save
+student_three = Student.create(first_name: "Bob", last_name: "Doe")
+student_three.save
+student_four = Student.create(first_name: "Liz", last_name: "Doe")
+student_four.save
 puts "students created"
 
 # Course.destroy_all
 
-# course_A1 = Course.create(title: "A1", description: "German Course for absolute beginners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
-# course_A1.save!
-# course_A2 = Course.create(title: "A2", description: "German Course for elementary level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
-# course_A2.save!
-# course_B1 = Course.create(title: "B1", description: "German Course for intermediate level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
-# course_B1.save!
-# course_B2 = Course.create(title: "B2", description: "German Course for upper-intermediate level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
-# course_B2.save!
-# course_C1 = Course.create(title: "C1", description: "German Course for advanced level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
-# course_C1.save!
-# course_C2 = Course.create(title: "C2", description: "German Course for advanced level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
-# course_C2.save!
+course_A1 = Course.create(title: "A1", description: "German Course for absolute beginners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
+course_A1.save!
+course_A2 = Course.create(title: "A2", description: "German Course for elementary level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
+course_A2.save!
+course_B1 = Course.create(title: "B1", description: "German Course for intermediate level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
+course_B1.save!
+course_B2 = Course.create(title: "B2", description: "German Course for upper-intermediate level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
+course_B2.save!
+course_C1 = Course.create(title: "C1", description: "German Course for advanced level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
+course_C1.save!
+course_C2 = Course.create(title: "C2", description: "German Course for advanced level learners", start_date: DateTime.parse("2021/8/01"), end_date: DateTime.parse("2021/8/27"), start_time: DateTime.parse("09:00"), end_time: DateTime.parse("12:30"), price: 420, shift: "Morning", user_id: 1, teacher_id: 1, student_id: 1)
+course_C2.save!
 puts "courses created!"
+
+room_one = Room.create(title: "Akkusativ")
+room_one.save
+room_two = Room.create(title: "Dative")
+room_two.save
+room_three = Room.create(title: "The Awful German language")
+room_three.save
+room_four = Room.create(title: "Stammtisch")
+room_four.save
+room_three = Room.create(title: "Feirabendbier")
+room_three.save
+room_four = Room.create(title: "Kopfkino")
+room_four.save
+
